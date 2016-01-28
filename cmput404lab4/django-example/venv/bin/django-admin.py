916dad5d376068d3e23455bb0b0f9779fe585184
sjpartri <sjpartri@ualberta.ca>
2016-01-27 18:58:28 -0700

    lab4

---
+++ cmput404lab4/django-example/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/cshome/sjpartri/Desktop/CMPUT404/cmput404Labs/cmput404lab4/django-example/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
